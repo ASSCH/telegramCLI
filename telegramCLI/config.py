68c510bcaac14ac13a17ee7caedf29c81fa56b4a
Stalkerik_228 <136360923+ASSCH@users.noreply.github.com>
2025-04-19 07:34:34 +0200

    Delete telegramCLI directory

--- telegramCLI/config.py
+++
@@ -1,8 +0,0 @@
-BOT_TOKEN = "ТОКЕН"
-CONTACTS_FILE = "contacts.json"
-HISTORY_DIR = "history"
-LANG = "ru_RU.UTF-8"
-MUSIC = {
-    "start": "assets/o-privet.mp3",
-    "exit": "assets/da-poshiol-ty-nakhui.mp3"
-}
